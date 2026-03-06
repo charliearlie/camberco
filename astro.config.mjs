b6431af95f46d735f715b683d83a46ed281abd9c
Charlie W <cw5790@gmail.com>
2026-03-06 16:23:27 +0000

    Use vercel astro packagr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://camberco.uk',
   integrations: [sitemap()],
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel(),
 });
